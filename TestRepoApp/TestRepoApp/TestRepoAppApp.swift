d0d2872e31ebe0c9a256757b0b7c4d8c322f923c
1k_mile <kamafeed1212@gmail.com>
2025-07-18 12:11:56 +0300

    init project

---
+++ TestRepoApp/TestRepoApp/TestRepoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestRepoAppApp.swift
+//  TestRepoApp
+//
+//  Created by Камил Хайрутдинов on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestRepoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
